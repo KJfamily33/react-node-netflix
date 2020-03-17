5cb248a36ecb4fc868fb61b0a1719911bd0619b6
Dan <danielzuzevich@gmail.com>
2020-03-17 18:19:26 -0400

    part-1 done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Global, css, jsx } from '@emotion/core'
-import reactLogo from '../img/react.png'
+import Navbar from './Navbar'
+import Jumbotron from './Jumbotron'
 
 /**
  * @function App
@@ -8,10 +9,10 @@ import reactLogo from '../img/react.png'
 const App = () => (
   <>
     <Global styles={GlobalCSS} />
+    <Navbar />
+    <Jumbotron />
 
-    {/* Delete me and the default .app styles in GlobalCSS */}
-    <img src={reactLogo} />
-    <h1>Hello Moto.</h1>
+    <div style={{ height: '600px' }} />
   </>
 )
 
@@ -20,37 +21,43 @@ const GlobalCSS = css`
     box-sizing: border-box;
     font-family: 'Roboto', sans-serif;
   }
-
   html,
   body,
   .app {
     margin: 0;
-    height: 100%;
+    min-height: 100%;
     width: 100%;
   }
-
-  // Just to position the react logo on initial startup. Delete this block.
-  .app {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    background: #282c34;
-
-    h1 {
-      color: white;
-    }
+  body {
+    background: #151515;
   }
-  //
-
   a {
     text-decoration: none;
+    color: white;
   }
-
+  p {
+    font-size: 20px;
+  }
   ul {
     margin: 0;
     list-style: none;
     padding: 0;
+  }
+  button {
+    background-color: rgba(51, 51, 51, 0.8);
+    border: 1px solid white;
+    padding: 0.75em 2.3em;
+    border-radius: 0.2vw;
+    box-shadow: none;
+    font-size: 1.1vw;
+    color: white;
+    margin-right: 15px;
+    cursor: pointer;
+    font-weight: 600;
+    letter-spacing: 0.4px;
+  }
+  i {
+    font-size: 18.5px;
   }
 `
 
